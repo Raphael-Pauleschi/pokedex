cef486d9c89442fbbb49d0298929940caba7baeb
raphael-pauleschi <raphael.f.pauleschi@gmail.com>
2023-02-14 19:51:22 -0300

    Images

--- src/components/PokeCard/index.jsx
+++ src/components/PokeCard/index.jsx
@@ -5,15 +5,15 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 
-export default function PokeCard({name}) {
+export default function PokeCard({name,image}) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
         <CardMedia
           component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
+          height="200"
+          image={image}
+          alt= {name}
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,20 +11,30 @@ export const Home = () => {
         getPokemons();
     });
     const getPokemons = () => {
+        var endpoints = [];
+        for(var i=906;i<1009; i++){
+            endpoints.push('https://pokeapi.co/api/v2/pokemon/'+i+'/')
+        }
+
+        var response = axios.all(endpoints.map((endpoint) => axios.get(endpoint)))
+        .then((res) => setPokemons(res));
+        return response;
+        /*
         axios
             .get("https://pokeapi.co/api/v2/pokemon?limit=103&offset=905")
             .then((res) => setPokemons(res.data.results))
             .catch((err) => console.log(err));
+           */ 
     }
 
     return (
         <div>
             <Navbar />
             <Container maxWidth='false'>
-                <Grid container>
+                <Grid container spacing={2}>
                     {pokemons.map((pokemon, key) => (
-                        <Grid item xs={3} key={key}>
-                            <PokeCard name={pokemon.name}/>
+                        <Grid item xs={2} key={key}>
+                            <PokeCard name={pokemon.data.name} image={pokemon.data.sprites.front_default}/>
                         </Grid>))}
                 </Grid>
             </Container>
