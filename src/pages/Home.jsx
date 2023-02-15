bf35cd16c7cea6c2117d972b7a9bc105cfe9a864
raphael-pauleschi <raphael.f.pauleschi@gmail.com>
2023-02-15 14:45:50 -0300

    Skeleton and select

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
-import { Grid } from "@mui/material";
+import { Grid} from "@mui/material";
 import { Container } from "@mui/system";
 import axios from "axios";
 import React, { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar';
 import PokeCard from '../components/PokeCard';
+import Skeletons from '../components/Skeletons';
 
 export const Home = () => {
     const [pokemons, setPokemons] = useState([]);
@@ -38,7 +39,8 @@ export const Home = () => {
             <Navbar pokeFilter ={pokeFilter} />
             <Container maxWidth='false'>
                 <Grid container spacing={2}>
-                    {pokemons.map((pokemon, key) => (
+                    {pokemons.length === 0 ? <Skeletons/>: 
+                    pokemons.map((pokemon, key) => (
                         <Grid item xs={12} sm={6} md={4} lg={2} key={key}>
                             <PokeCard name={pokemon.data.name} image={pokemon.data.sprites.front_default} types={pokemon.data.types} />
                         </Grid>))}
