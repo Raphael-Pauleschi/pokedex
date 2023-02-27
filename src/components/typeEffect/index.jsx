7284271d691e614c8699074c5431d73d40c62e57
raphael-pauleschi <raphael.f.pauleschi@gmail.com>
2023-02-27 15:01:19 -0300

    Problem with monotype solved

--- src/components/typeEffect/index.jsx
+++ src/components/typeEffect/index.jsx
@@ -3,7 +3,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import axios from 'axios';
 
-export default function TypeEffect({ type1, type2, teraType }) {
+export default function TypeEffect({ type1, type2 }) {
     const [infoType1, setInfoType1] = useState([]);
     const [infoType2, setInfoType2] = useState([]);
     const [quadrupleDamageFrom, setQuadrupleDamageFrom] = useState([]);
@@ -11,14 +11,15 @@ export default function TypeEffect({ type1, type2, teraType }) {
     const [halfDamageFrom, setHalfDamageFrom] = useState([]);
     const [quarterDamageFrom, setQuarterDamageFrom] = useState([]);
     const [noDamageFrom, setNoDamageFrom] = useState([]);
-
+    
+    if(type2){
     useEffect(() => {
         const fetchTypeInfo = async () => {
             try {
                 const response1 = await axios.get(`https://pokeapi.co/api/v2/type/${type1}`);
                 const data1 = response1.data.damage_relations;
                 setInfoType1(data1);
-
+                
                 if (type2) {
                     const response2 = await axios.get(`https://pokeapi.co/api/v2/type/${type2}`);
                     const data2 = response2.data.damage_relations;
@@ -35,17 +36,11 @@ export default function TypeEffect({ type1, type2, teraType }) {
 
     useEffect(() => {
         const damageHandle = () => {
-            console.log(infoType1);
-            console.log(infoType2);
+            console.log(type1);
             const newDoubleDamageFromType1 = infoType1.double_damage_from.map(relation => relation.name);
             const newHalfDamageFromType1 = infoType1.half_damage_from.map(relation => relation.name);
             const newNoDamageFromType1 = infoType1.no_damage_from.map(relation => relation.name);
 
-            if (!infoType2) {
-                setDoubleDamageFrom(newDoubleDamageFromType1);
-                setHalfDamageFrom(newHalfDamageFromType1);
-                setNoDamageFrom(newNoDamageFromType1);
-            } else {
                 const newDoubleDamageFromType2 = infoType2.double_damage_from.map(relation => relation.name);
                 const newHalfDamageFromType2 = infoType2.half_damage_from.map(relation => relation.name);
                 const newNoDamageFromType2 = infoType2.no_damage_from.map(relation => relation.name);
@@ -98,13 +93,45 @@ export default function TypeEffect({ type1, type2, teraType }) {
                         )
                     )
                 );
-            }
         };
-
         if (infoType1.double_damage_from && (!infoType2 || infoType2.double_damage_from)) {
             damageHandle();
         }
     }, [infoType1, infoType2]);
+    }else{
+            useEffect(() => {
+                const fetchTypeInfo = async () => {
+                    try {
+                        const response1 = await axios.get(`https://pokeapi.co/api/v2/type/${type1}`);
+                        const data1 = response1.data.damage_relations;
+                        setInfoType1(data1);
+        
+                    } catch (error) {
+                        console.log(error);
+                    }
+                };
+        
+                fetchTypeInfo();
+            }, [type1, type2]);
+        
+            useEffect(() => {
+                const damageHandle = () => {
+                    console.log(type1);
+                    const newDoubleDamageFromType1 = infoType1.double_damage_from.map(relation => relation.name);
+                    const newHalfDamageFromType1 = infoType1.half_damage_from.map(relation => relation.name);
+                    const newNoDamageFromType1 = infoType1.no_damage_from.map(relation => relation.name);
+        
+                   
+                        setDoubleDamageFrom(newDoubleDamageFromType1);
+                        console.log(doubleDamageFrom);
+                        setHalfDamageFrom(newHalfDamageFromType1);
+                        setNoDamageFrom(newNoDamageFromType1);
+                          
+            };if (infoType1.double_damage_from) {
+                damageHandle();
+            }  }, [infoType1]);
+    }
+
 
     function typeShow(typeArray) {
         return typeArray.map((weak) => {
